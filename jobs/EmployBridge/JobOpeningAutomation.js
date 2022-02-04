8a0aab2e63faa51293b76845c8b57f99857a21cc
mauriciosixcel <mmontoya@sixcel.com>
2022-02-03 20:57:45 -0500

    updates

--- jobs/EmployBridge/JobOpeningAutomation.js
+++ jobs/EmployBridge/JobOpeningAutomation.js
@@ -1,15 +1,29 @@
 // SWE-128:  Job Opening Automation
 // Name: Belflex Customization: Populate Job Owner from Account Owner at Company
 // Entity/Tracks:  ClientCompany
-// Fields: customInt1, customText4, customInt2, customDate1, customText12
+// Fields: customInt4, customInt2
+// Event: Fi –On init
+
+console.log('SWE-128:  Job Opening Automation ', API);
+const customInt4 = API.form.controls['customInt4'].value
+const customInt2 = API.form.controls['customInt2'].value
+if (customInt2 === null)
+    API.setValue('customInt2', customInt4)
+
+
+// SWE-128:  Job Opening Automation
+// Name: Belflex Customization: Populate Job Owner from Account Owner at Company
+// Entity/Tracks:  ClientCompany
+// Fields: customInt4, customInt2
 // Event: Fi –On change
 
-const clientID = API.form.controls.clientCorporation.value.id
-const getCompany = `/entity/ClientCorporation/${clientID}?fields=id,customText10`
-return API.appBridge.httpGET(getCompany)
-    .then(resp => {
-        if (resp.data.data.customText10 !== '') {
-            API.setValue('owner', resp.data.data.customText10)
-        }
-    })
-    .catch(err => console.log(err))
+console.log('SWE-128:  Job Opening Automation ', API);
+const customInt4 = API.form.controls['customInt4'].value
+const customInt2 = API.form.controls['customInt2'].value
+if (customInt2 > customInt4) {
+    API.hide('customText8');
+    API.markAsInvalid(API.getActiveKey(), '# Committed cannot exceed Total Client Need');
+} else {
+    API.show('customText8');
+    API.setRequired('customText8', true);
+}
