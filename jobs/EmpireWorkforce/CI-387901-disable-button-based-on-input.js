49798f71c733fa24ce4c8dfd9057e0b0ed274aa4
mmontoya@sixcel.com <mmontoya@sixcel.com>
2022-05-04 23:31:57 -0500

    updates

---
+++ jobs/EmpireWorkforce/CI-387901-disable-button-based-on-input.js
@@ -0,0 +1,51 @@
+// SWE-345: Empire Workforce Customization - Validate Candidate Address for Prism Integration
+// Name: Empire Workforce Customization - Validate Candidate Address for Prism Integration
+// Type: Start/End Date Automation
+// Entity/Tracks: Candidate
+// Fields: address1,address2,zip,state
+// Event: Add Edit Presave
+
+if (API.currentEntity === "Candidate") {
+  let address1 = form.controls["address"].value.address1;
+  let zip = form.controls["address"].value.zip;
+  let state = form.controls["address"].value.state;
+  return new Promise((resolve) => {
+    const validateAddres = address1?.split(" ").find((element) => {
+      if (
+        [
+          "PO",
+          "PO BOX",
+          "P.O",
+          "P.O.",
+          "PO.BOX",
+          "P.O.BOX",
+          "PO.Box",
+          "P.O.Box",
+          "POBOX",
+        ].includes(element)
+      )
+        return element;
+    });
+    if (validateAddres !== undefined) {
+      (form.errorMessage =
+        "PO Box address are not allowed within the Candidate permanent Address field"),
+        (form.isFormValid = false);
+      resolve(form);
+      return;
+    } else if ((zip !== null && zip.length > 5) || zip === null) {
+      (form.errorMessage = "Zip Code must be 5 digits"),
+        (form.isFormValid = false);
+      resolve(form);
+      return;
+    } else if (state === "" || state === null) {
+      (form.errorMessage =
+        "State is missing or formatted incorrectly. Please select a state from the drop down."),
+        (form.isFormValid = false);
+      resolve(form);
+      return;
+    }
+    form.isFormValid = true;
+    resolve(form);
+    return;
+  });
+}
---
+++ jobs/EmpireWorkforce/SWE-342-Populate-EEO-field-for-Prism.js
@@ -0,0 +1,21 @@
+if (API.currentEntity === "Placement") {
+  const workersCompRateID = form.controls.workersCompensationRate.value.id;
+  const employeeOTType =
+    form.controls["timeAndExpense.employeeOtType"].value === 3 ? "Y" : "N";
+  const correlatedCustomText5 = form.controls.correlatedCustomText5.value;
+  const Placement = `/entity/Placement/${API.currentEntityId}`;
+
+  console.log("workersCompRateID ", workersCompRateID);
+  console.log("employeeOTType ", employeeOTType);
+  console.log("correlatedCustomText5 ", correlatedCustomText5);
+  let customText1 = "";
+  let Obj = {
+    customText1: customText1.concat(
+      workersCompRateID,
+      employeeOTType,
+      correlatedCustomText5
+    ),
+  };
+
+  return API.appBridge.httpPOST(Placement, Obj);
+}
